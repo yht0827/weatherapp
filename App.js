62016c7cde2b366f7b628ebf5ef14e2105512dba
yht0827 <yangheetae0827@naver.com>
2019-03-03 17:47:17 +0900

    commit first

---
+++ App.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
+import { red } from 'ansi-colors';
+
+export default class App extends Component {
+
+  state={
+    isLoaded: false
+  }
+
+  render() {
+
+    const {isLoaded} = this.state;//false
+
+    return (
+      <View style={styles.container}>
+      { isLoaded ? null : <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the fucking weather</Text>
+        </View>}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  loading: {
+    flex:1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText:{
+   fontSize:38,
+   marginBottom:100,
+  }
+
+
+
+});
---
+++ weather.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import { StyleSheet, View, Text} from "react-native";
+import {LinearGradient} from "expo";
+
+export default class Weather extends Component{
+
+    render(){
+        <LinearGradient>
+        </LinearGradient>
+
+    }
+
+}
