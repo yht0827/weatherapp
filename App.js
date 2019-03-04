cb612b0a4fbb087e15cd3c40e30a917167281be0
yht0827 <yangheetae0827@naver.com>
2019-03-04 19:52:46 +0900

    done

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
-import { red } from 'ansi-colors';
+import { StyleSheet, Text, View,StatusBar} from 'react-native';
+import Weather from "./weather";
+
+const API_KEY="de892361d6cf402ea35640c5df8fbfd2";
 
 export default class App extends Component {
 
-  state={
-    isLoaded: false
+  state = {
+    isLoaded: false,
+    error:null,
+    temperature:null,
+    name:null,
+    dong:null,
+    country:null
+  };
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude,position.coords.longitude);
+        },
+        error => {
+          this.setState({
+              error:error
+          });
+        }
+        );
+  }
+
+  _getWeather= (lat,long) =>{
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}&lang=Kr`)
+    .then(response => response.json())
+    .then(json =>{
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        dong:json.name,
+        country:json.sys.country,
+        isLoaded:true,
+      })
+    });
   }
 
   render() {
 
-    const {isLoaded} = this.state;//false
+    const { isLoaded,error,temperature,name,dong,country} = this.state;//false
 
     return (
       <View style={styles.container}>
-      { isLoaded ? null : <View style={styles.loading}>
-          <Text style={styles.loadingText}>Getting the fucking weather</Text>
-        </View>}
+      <StatusBar hidden={true} />
+      { isLoaded ? 
+        (<Weather weatherName={name} temp={Math.ceil(temperature - 273.15)} dong={dong} country={country} />): 
+      (
+          <View style={styles.loading}>
+          <Text style={styles.loadingText}>날씨를 불러오는중...</Text>
+          {error ? <Text style={styles.errorText}>{error}</Text> :  null}
+        </View>
+      )}
       </View>
     );
   }
@@ -27,6 +67,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+    marginBottom:40
+  },
   loading: {
     flex:1,
     backgroundColor: "#FDF6AA",
@@ -34,10 +79,8 @@ const styles = StyleSheet.create({
     paddingLeft: 25
   },
   loadingText:{
+    color:"white",
    fontSize:38,
-   marginBottom:100,
+   marginBottom:30,
   }
-
-
-
 });
--- weather.js
+++ weather.js
@@ -1,13 +1,143 @@
 import React,{Component} from "react";
 import { StyleSheet, View, Text} from "react-native";
 import {LinearGradient} from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component{
+/* export default class Weather extends Component{
 
     render(){
-        <LinearGradient>
+       return( <LinearGradient 
+        colors={["#00C6FB","#005BEA"]} 
+        style={styles.container}>
+        <View style={styles.upper}>
+            <Ionicons color="white" size={144} name="ios-rainy"/>
+            <Text style={styles.temp}>35º</Text>
+        </View>
+        <View style={styles.lower}>
+            <Text style={styles.title}>Raining like a MF</Text>
+            <Text style={styles.subtitle}>For more info look outside</Text>
+        </View>
         </LinearGradient>
-
+        );
     }
+} */
 
+const weatherCases = {
+    Rain:{
+        colors:['#00C6FB', "#005BEA"],
+        title:"비가 온다.",
+        subtitle:"더 자세한 날씨는 창 밖을 봐라.",
+        icon:"weather-rainy"
+    },
+    Clear:{
+        colors:['#FEF253', "#FF7300"],
+        title:"날씨가 맑다.",
+        subtitle:"지금이 나가야 놀 수있다.",
+        icon:"weather-sunny"
+    },
+    Thunderstorm:{
+        colors:["#00ECBC", "#007ADF"],
+        title:"천둥이 친다.",
+        subtitle:"지금이 나가면 벼락 맞을 확률이 높다.",
+        icon:"weather-lightning"
+    },
+    Clouds:{
+        colors:["#D7D2CC", "#304352"],
+        title:"구름이 많다.",
+        subtitle:"하늘을 보고 구름을 세어보자.",
+        icon:"weather-cloudy"
+    },
+    Snow:{
+        colors:["#7DE2FC", "#B9B6E5"],
+        title:"눈이 내린다.",
+        subtitle:"눈을 맞아보자.아픈지 테스트해봐.",
+        icon:"weather-snowy"
+    },
+    Drizzle:{
+        colors:["#89F7FE", "#66A6FF"],
+        title:"이슬비가 내린다.",
+        subtitle:"우산을 챙길까? 말까? 챙겨라.",
+        icon:"weather-hail"
+    },
+    Haze:{
+        colors:["#d9dde0", "#dae3ed"],
+        title:"안개가 많다.",
+        subtitle:"앞에 뭐가 보이는지 보이니?",
+        icon:"weather-fog"
+    }
+  }
+
+function Weather({weatherName,temp,dong,country}) { //stateless
+return(
+    <LinearGradient     
+    colors={weatherCases[weatherName].colors} 
+    style={styles.container}>
+    <View style={styles.upper}>
+        <MaterialCommunityIcons color="white" size={144} name={weatherCases[weatherName].icon}/>
+        <Text style={styles.temp}>{temp}º</Text>
+    </View>
+    <View style={styles.center}>
+          <Text style={styles.loc}>{dong}, {country}</Text>
+    </View>
+    <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
+    </View>
+    </LinearGradient>
+    ); 
 }
+Weather.propTypes = {
+    temp:PropTypes.number.isRequired,
+    weatherName:PropTypes.string.isRequired,
+    dong:PropTypes.string.isRequired,
+    country:PropTypes.string.isRequired
+  };
+
+export default Weather;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+    upper:{
+        flex:6,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    temp:{
+        fontSize:48,
+        backgroundColor:"transparent",
+        color:"white",
+        marginTop:10
+    },
+    center:{
+        flex:3,
+        alignItems:"center"
+    },
+    loc:{
+        fontSize:25,
+        backgroundColor:"transparent",
+        color:"white",
+    },
+    lower:{
+        flex:1,
+        alignItems:"flex-start",
+        backgroundColor: "transparent",
+        justifyContent:"flex-end",
+        paddingLeft:25
+    },
+    title: {
+    fontSize:38,
+       backgroundColor: "transparent",
+       color:"white",    
+       marginBottom:10,
+       fontWeight: "300" 
+    },
+    subtitle:{
+        fontSize:24,
+       backgroundColor: "transparent",
+       color:"white",
+       marginBottom:50 
+    }
+});
